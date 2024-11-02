5263e6c6e72240aaf3579d169b1e6f4f56fea162
ZAWA <k.kai1018@icloud.com>
2024-11-02 11:30:39 +0900

    勝敗追加

--- app5.js
+++ app5.js
@@ -29,8 +29,8 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
+  let win = Number( req.query.win )|| 0;
+  let total = Number( req.query.total )|| 0;
   console.log( {hand, win, total});
   const num = Math.floor( Math.random() * 3 + 1 );
   let cpu = '';
@@ -38,16 +38,32 @@ app.get("/janken", (req, res) => {
   else if( num==2 ) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
+  if ((hand == 'グー' && cpu == 'チョキ') || 
+    (hand == 'チョキ' && cpu == 'パー') || 
+    (hand == 'パー' && cpu == 'グー')) {
+  judgement = '勝ち';
   win += 1;
-  total += 1;
+} else if (hand === cpu) {
+  judgement = '引き分け';
+} else {
+  judgement = '負け';
+}
+total += 1;
+
+
+
+  // 今はダミーで人間の勝ちにしておく
+  //let judgement = '勝ち';
+  //win += 1;
+  //total += 1;
+  
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
-    total: total
+    total: total,
   }
   res.render( 'janken', display );
 });
